8a70a71a94d71c370a71e95e21d88a183fafcf1b
Samp <patelbhavik89@gmail.com>
2018-01-11 15:13:20 -0500

    initial Commit

---
+++ src/main/java/com/example/bah/helloworld/App.java
@@ -0,0 +1,27 @@
+package com.example.bah.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+
+import java.util.logging.Logger;
+
+
+
+public class App
+{
+    private static Logger logger = Logger.getLogger("App.class");
+
+    public static void main( String[] args ) {
+        for (int i = 0; i < 10; i++) {
+
+            logger.info("Welcome to BAH Bhavik!");
+        }
+    }
+    public void main1(  )
+    {
+
+        logger.info("Welcome to BAH Bhavik!");
+    }
+}
